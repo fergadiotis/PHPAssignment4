4392c6c6e71974d2eb11bec842a639ed2f28ebb3
fergadiotis <fergadiotis@gmail.com>
2024-10-14 22:06:56 -0400

    Uploaded 15-2 on 6-3 project

---
+++ PHPAssignment4/tech_support/index.php
@@ -0,0 +1,32 @@
+<?php include 'view/header.php'; ?>
+<main>
+    <nav>
+    <div class="admin">
+    <h2>Administrators</h2>
+    <ul>
+        <li><a href="product_manager">Manage Products</a></li>
+        <li><a href="under_construction.php">Manage Technicians</a></li>
+        <li><a href="under_construction.php">Manage Customers</a></li>
+        <li><a href="under_construction.php">Create Incident</a></li>
+        <li><a href="under_construction.php">Assign Incident</a></li>
+        <li><a href="under_construction.php">Display Incidents</a></li>
+    </ul>
+    </div>
+
+    <div class="tech">
+    <h2>Technicians</h2>    
+    <ul>
+        <li><a href="under_construction.php">Update Incident</a></li>
+    </ul>
+    </div>
+
+    <div class="customer">
+    <h2>Customers</h2>
+    <ul>
+        <li><a href="under_construction.php">Register Product</a></li>
+    </ul>
+    </div>
+    
+    </nav>
+</main>
+<?php include 'view/footer.php'; ?>
